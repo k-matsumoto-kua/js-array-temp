0b0680dfab12a1e82ca2c691b53e92ec6b5ec839
k-matsumoto-kua <k-matsumoto@kua.kyoto-art.ac.jp>
2020-10-30 13:21:48 +0900

    first commit

---
+++ sample/js/script02.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  let students = ["田中","山田","松本","山本","沢田"]
+
+  $("#button00").on("click",function(){
+    console.log(students)
+  })
+  
+
+
+
+
+
+})
---
+++ sample/js/script03.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  let students = [
+    {name:"田中",age:24,sport:"サッカー"},
+    {name:"山田",age:22,sport:"野球"},
+    {name:"松本",age:40,sport:"バスケットボール"},
+    {name:"山本",age:19,sport:"テニス"},
+    {name:"沢田",age:22,sport:"サッカー"}
+  ]
+
+  $("#button00").on("click",function(){
+    console.log(students)
+  })
+
+
+
+
+})
